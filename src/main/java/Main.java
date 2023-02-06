071003a58b2edcff7cf9b923a32740757f8eec90
unknown <oberten@mail.ru>
2023-02-06 22:12:25 +0300

    add code

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        FormDate birthday = new FormDate();
+        {
+            birthday.day = 13;
+            birthday.month = 6;
+            birthday.year = 1999;
+        }
+        // заполните другие поля даты рождения
+
+
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7(999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = "true";
+
+
+        // заполните другие поля
+    }
+}
+
---
+++ src/main/java/Post.java
@@ -0,0 +1,14 @@
+public class Post {
+
+    public String day;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+
+
+    // поле для имени
+    // добавьте другие поля
+}
