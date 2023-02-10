2e8000630a7c8846bca59b10defc5a4d6766655b
unknown <oberten@mail.ru>
2023-02-10 20:18:22 +0300

    add code 2

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -6,7 +6,7 @@ public class Post {
     public String patronymic;
     public String phone;
     public String surname;
-    public String subscription;
+    public Boolean subscription;
 
 
     // поле для имени
